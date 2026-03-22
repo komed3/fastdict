30f9f220ffdb55c8696e8e31829023daa627c54c
komed3 <pk97@outlook.de>
2026-03-22 09:54:10 +0100

    add Hasher.fastFNV1a

--- src/Hasher.ts
+++ src/Hasher.ts
@@ -1,1 +1,10 @@
-export class Hasher {}
+export class Hasher {
+
+    public static fastFNV1a ( str: string ) : number {
+        const len = str.length;
+        const limit = len & ~3;
+        let hash = 0x01000193;
+        let i = 0;
+    }
+
+}
