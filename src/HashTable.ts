148602b6a4dd8f595cdaf6526cba024af5390716
komed3 <pk97@outlook.de>
2026-03-22 15:57:41 +0100

    implement hash table options

--- src/HashTable.ts
+++ src/HashTable.ts
@@ -5,24 +5,32 @@ export type Hash = 'fasthash' | 'fnv1a' | 'murmur3' | HashFn;
 
 export interface HashOptions {
     hash?: Hash;
+    seed?: number;
+    maxStrLen?: number;
+    fifo?: boolean;
+    maxSize?: number;
 }
 
 
 const DEFAULT_OPTIONS : HashOptions = {
-    hash: 'fasthash'
+    hash: 'fasthash',
+    maxStrLen: 2048,
+    fifo: true,
+    maxSize: 10_000
 };
 
 
 export class HashTable {
 
     private readonly options: HashOptions;
-    private readonly hash: HashFn;
+    private readonly hashFn: HashFn;
+
     private table = new Map< string, any > ();
 
     constructor ( options: HashOptions = {} ) {
         this.options = { ...DEFAULT_OPTIONS, ...options };
 
-        try { this.hash = typeof this.options.hash === 'function' ? this.options.hash
+        try { this.hashFn = typeof this.options.hash === 'function' ? this.options.hash
             : this.options.hash = Hasher[ this.options.hash as keyof Hasher ] }
         catch ( err ) { throw Error ( `Cannot set hasher function`, { cause: err } ) }
     }
