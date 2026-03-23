a308debbf9b27d0032605d581f8c594bc296ea5c
komed3 <pk97@outlook.de>
2026-03-23 18:50:46 +0100

    Update hash.ts

--- tests/hash.ts
+++ tests/hash.ts
@@ -1,12 +1,12 @@
-import { HashTable } from '../src/HashTable.ts';
+import { Dict } from '../src/Dict.ts';
 
-const table = new HashTable( { hash: 'fnv1a' } );
+const dict = new Dict( { hash: 'fnv1a' } );
 const arr = [ 'Hello World', 'Some test', 'Another test', 'Hello World' ];
 
 for ( const str of arr ) {
-    const key = table.key( [ str ] );
-    if ( key && table.has( key ) ) console.log( 'Duplicate:', str );
-    else if ( key ) table.set( key, str );
+    const key = dict.key( [ str ] );
+    if ( key && dict.has( key ) ) console.log( 'Duplicate:', str );
+    else if ( key ) dict.set( key, str );
 }
 
-console.log( table.size() );
+console.log( dict.size() );
